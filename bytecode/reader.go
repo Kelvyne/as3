743aa2189160e2ff042189399da42c342d307aef
Lakhdar Slaim <lakhdar.slaim@epitech.eu>
2016-12-23 10:25:46 -0500

    as3 reader

---
+++ bytecode/reader.go
@@ -0,0 +1,105 @@
+package bytecode
+
+import "io"
+import "encoding/binary"
+import "errors"
+
+// ErrMalformedVariableInteger means that an encoded variable integer is
+// malformed (its length is >5 bytes)
+var ErrMalformedVariableInteger = errors.New("malformed variable integer")
+
+// Reader is the minimal interface required to read as3 bytecode
+type Reader interface {
+	ReadU8() (uint8, error)
+	ReadU16() (uint16, error)
+	ReadS24() (int32, error)
+	ReadU30() (uint32, error)
+	ReadU32() (uint32, error)
+	ReadS32() (int32, error)
+	ReadD64() (float64, error)
+}
+
+type reader struct {
+	io.Reader
+}
+
+// NewReader provides a simple way to create an as3 bytecode reader
+func NewReader(r io.Reader) Reader {
+	return &reader{r}
+}
+
+func (r *reader) read(data interface{}) error {
+	return binary.Read(r.Reader, binary.LittleEndian, data)
+}
+
+func (r *reader) ReadU8() (uint8, error) {
+	var v uint8
+	err := r.read(&v)
+	return v, err
+}
+
+func (r *reader) ReadU16() (uint16, error) {
+	var v uint16
+	err := r.read(&v)
+	return v, err
+}
+
+func (r *reader) ReadS24() (int32, error) {
+	var bytes [3]byte
+	err := r.read(bytes[:])
+	if err != nil {
+		return 0, err
+	}
+	v := uint32(bytes[0]) | uint32(bytes[1])<<8 | uint32(bytes[2])<<16
+	if v>>23 != 0 {
+		v |= 0xff000000
+	}
+	return int32(v), nil
+}
+
+func (r *reader) readVariableLength() (v uint32, n uint8, err error) {
+	var b uint8 = 0x80
+	for b>>7 != 0 {
+		b, err = r.ReadU8()
+		if err != nil {
+			return
+		}
+		v |= (uint32(b & 0x7f)) << (n * 7)
+		n++
+		if n >= 5 {
+			err = ErrMalformedVariableInteger
+			v = 0
+			return
+		}
+	}
+	return
+}
+
+func (r *reader) ReadU32() (uint32, error) {
+	v, _, err := r.readVariableLength()
+	return v, err
+}
+
+func (r *reader) ReadU30() (uint32, error) {
+	return r.ReadU32()
+}
+
+func (r *reader) ReadS32() (int32, error) {
+	v, n, err := r.readVariableLength()
+	if err != nil {
+		return 0, err
+	}
+	// If the higher bit of the last read byte is 1, it means we need to
+	// expand the sign (v is negative)
+	if v&(1<<(n*7-1)) != 0 {
+		// shift should be n*7 + 1 but we know n*7th bit is set
+		v |= 0xffffffff << (n * 7)
+	}
+	return int32(v), nil
+}
+
+func (r *reader) ReadD64() (float64, error) {
+	var v float64
+	err := r.read(&v)
+	return v, err
+}
---
+++ bytecode/reader_test.go
@@ -0,0 +1,347 @@
+package bytecode
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func TestNewReader(t *testing.T) {
+	r := NewReader(nil)
+	if r == nil {
+		t.Fatalf("expected non-nil, got %v", r)
+	}
+}
+
+func Test_reader_ReadU8(t *testing.T) {
+	type fields struct {
+		Reader io.Reader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    uint8
+		wantErr bool
+	}{
+		{
+			"valid",
+			fields{bytes.NewReader([]byte{0x5a})},
+			0x5a,
+			false,
+		},
+		{
+			"EOF",
+			fields{&bytes.Buffer{}},
+			0,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &reader{
+				Reader: tt.fields.Reader,
+			}
+			got, err := r.ReadU8()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("reader.ReadU8() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("reader.ReadU8() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reader_ReadU16(t *testing.T) {
+	type fields struct {
+		Reader io.Reader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    uint16
+		wantErr bool
+	}{
+		{
+			"valid",
+			fields{bytes.NewReader([]byte{0x5a, 0x8c})},
+			0x8c5a,
+			false,
+		},
+		{
+			"EOF",
+			fields{&bytes.Buffer{}},
+			0,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &reader{
+				Reader: tt.fields.Reader,
+			}
+			got, err := r.ReadU16()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("reader.ReadU16() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("reader.ReadU16() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reader_ReadS24(t *testing.T) {
+	type fields struct {
+		Reader io.Reader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    int32
+		wantErr bool
+	}{
+		{
+			"positive",
+			fields{bytes.NewReader([]byte{0x5a, 0x8c, 0x07})},
+			494682,
+			false,
+		},
+		{
+			"negative",
+			fields{bytes.NewReader([]byte{0xa6, 0x73, 0xf8})},
+			-494682,
+			false,
+		},
+		{
+			"EOF",
+			fields{&bytes.Buffer{}},
+			0,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &reader{
+				Reader: tt.fields.Reader,
+			}
+			got, err := r.ReadS24()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("reader.ReadS24() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("reader.ReadS24() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func Test_reader_ReadU32(t *testing.T) {
+	type fields struct {
+		Reader io.Reader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    uint32
+		wantErr bool
+	}{
+		{
+			"valid",
+			fields{bytes.NewReader([]byte{0x5a})},
+			0x5a,
+			false,
+		},
+		{
+			"multi bytes",
+			fields{bytes.NewReader([]byte{0xff, 0x03})},
+			0x1ff,
+			false,
+		},
+		{
+			"malformed variable length",
+			fields{bytes.NewReader([]byte{0x81, 0x82, 0x83, 0x84, 0x85, 0x86})},
+			0,
+			true,
+		},
+		{
+			"EOF",
+			fields{&bytes.Buffer{}},
+			0,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &reader{
+				Reader: tt.fields.Reader,
+			}
+			got, err := r.ReadU32()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("reader.ReadU32() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("reader.ReadU32() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reader_ReadU30(t *testing.T) {
+	type fields struct {
+		Reader io.Reader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    uint32
+		wantErr bool
+	}{
+		{
+			"valid",
+			fields{bytes.NewReader([]byte{0x5a})},
+			0x5a,
+			false,
+		},
+		{
+			"multi bytes",
+			fields{bytes.NewReader([]byte{0xff, 0x03})},
+			0x1ff,
+			false,
+		},
+		{
+			"malformed variable length",
+			fields{bytes.NewReader([]byte{0x81, 0x82, 0x83, 0x84, 0x85, 0x86})},
+			0,
+			true,
+		},
+		{
+			"EOF",
+			fields{&bytes.Buffer{}},
+			0,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &reader{
+				Reader: tt.fields.Reader,
+			}
+			got, err := r.ReadU30()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("reader.ReadU30() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("reader.ReadU30() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reader_ReadS32(t *testing.T) {
+	type fields struct {
+		Reader io.Reader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    int32
+		wantErr bool
+	}{
+		{
+			"valid",
+			fields{bytes.NewReader([]byte{0x5a})},
+			0x5a,
+			false,
+		},
+		{
+			"multi bytes",
+			fields{bytes.NewReader([]byte{0xff, 0x03})},
+			0x1ff,
+			false,
+		},
+		{
+			"negative",
+			fields{bytes.NewReader([]byte{0x7f})},
+			-1,
+			false,
+		},
+		{
+			"negative multi bytes",
+			fields{bytes.NewReader([]byte{0xff, 0x7f})},
+			-1,
+			false,
+		},
+		{
+			"malformed variable length",
+			fields{bytes.NewReader([]byte{0x81, 0x82, 0x83, 0x84, 0x85, 0x86})},
+			0,
+			true,
+		},
+		{
+			"EOF",
+			fields{&bytes.Buffer{}},
+			0,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &reader{
+				Reader: tt.fields.Reader,
+			}
+			got, err := r.ReadS32()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("reader.ReadS32() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("reader.ReadS32() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reader_ReadD64(t *testing.T) {
+	type fields struct {
+		Reader io.Reader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    float64
+		wantErr bool
+	}{
+		{
+			"valid",
+			fields{bytes.NewReader([]byte{0x5a, 0x8c, 0x5a, 0x8c, 0x5a, 0x8c, 0x5a, 0x8c})},
+			-3.7079989838049655e-249,
+			false,
+		},
+		{
+			"EOF",
+			fields{&bytes.Buffer{}},
+			0,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &reader{
+				Reader: tt.fields.Reader,
+			}
+			got, err := r.ReadD64()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("reader.ReadD64() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("reader.ReadD64() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
